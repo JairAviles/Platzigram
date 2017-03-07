c066aae83b51abcc490bd0adcb80801a4c5896ba
jair.aviles.plyom <jair.aviles@plyom.com.br>
2017-03-07 10:47:26 -0300

    add toolbar and create Account activity

--- app/src/main/java/br/com/plyom/plyomgram/view/CreateAccountActivity.java
+++ app/src/main/java/br/com/plyom/plyomgram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package br.com.plyom.plyomgram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import br.com.plyom.plyomgram.R;
 
@@ -11,6 +12,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_create_account), false);
     }
+
+    public void showToolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
 }
 
