0a06c0275e86a4d3ce98307b87f8e65bf618065d
Jair Avilés <hi@jairaviles.mx>
2017-05-03 22:14:53 -0300

    add home profile fragment

--- app/src/main/java/br/com/plyom/plyomgram/view/fragment/ProfileFragment.java
+++ app/src/main/java/br/com/plyom/plyomgram/view/fragment/ProfileFragment.java
@@ -2,11 +2,19 @@ package br.com.plyom.plyomgram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import br.com.plyom.plyomgram.R;
+import br.com.plyom.plyomgram.adapter.PictureAdapterRecyclerView;
+import br.com.plyom.plyomgram.model.Picture;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +31,39 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("", false, view);
+
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureProfileRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public void showToolbar(String title, boolean upButton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg", "Tiago Peixoto", "4 días", "3 Me gusta"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Bridge--by-Art-Fronckowiak-.jpg", "Jair Avilés", "3 días", "10 Me gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Aline Castro", "2 días", "9 Me gusta"));
+        pictures.add(new Picture("https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/16674/SITours/excurs-o-particular-ao-p-o-de-a-car-e-cristo-redentor-in-rio-de-janeiro-278478.jpg", "Luiz Figueiro", "9 días", "11 Me gusta"));
+        pictures.add(new Picture("https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/36292/SITours/private-st-petersburg-tour-with-boat-ride-in-saint-petersburg-344290.jpg", "Juan Perez", "4 días", "2 Me gusta"));
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Mumbai_03-2016_31_Gateway_of_India.jpg/220px-Mumbai_03-2016_31_Gateway_of_India.jpg", "Luana de souza", "6 días", "5 Me gusta"));
+
+        return pictures;
     }
 
 }
