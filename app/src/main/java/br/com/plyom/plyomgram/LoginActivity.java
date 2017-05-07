54c133c646b5c7e6e8a8d3ffa85eef1ad79178d2
Jair Avilés <hi@jairaviles.mx>
2017-05-06 22:49:17 -0300

    add Challenge 2, redirect to website when Clicking onLogo

--- app/src/main/java/br/com/plyom/plyomgram/LoginActivity.java
+++ app/src/main/java/br/com/plyom/plyomgram/LoginActivity.java
@@ -1,9 +1,11 @@
 package br.com.plyom.plyomgram;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import br.com.plyom.plyomgram.view.ContainerActivity;
 import br.com.plyom.plyomgram.view.CreateAccountActivity;
@@ -14,6 +16,7 @@ public class  LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        gotoWebSite();
     }
 
     public void goCreateAccount(View view) {
@@ -25,6 +28,21 @@ public class  LoginActivity extends AppCompatActivity {
     public void goHome(View view) {
         Intent i = new Intent(this, ContainerActivity.class);
         startActivity(i);
+    }
+
+    private void gotoWebSite() {
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://platzigram.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
