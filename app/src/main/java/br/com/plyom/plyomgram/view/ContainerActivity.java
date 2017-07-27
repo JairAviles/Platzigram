f168aa7ff7caa02c4a40829816a0d67685fd6008
Jair Aviles <jair.aviles@ahgora.com.br>
2017-07-27 16:24:13 -0300

    add options menu on container

--- app/src/main/java/br/com/plyom/plyomgram/view/ContainerActivity.java
+++ app/src/main/java/br/com/plyom/plyomgram/view/ContainerActivity.java
@@ -1,28 +1,45 @@
 package br.com.plyom.plyomgram.view;
 
+import android.content.Intent;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import br.com.plyom.plyomgram.R;
+import br.com.plyom.plyomgram.login.view.CreateAccountActivity;
+import br.com.plyom.plyomgram.login.view.LoginActivity;
 import br.com.plyom.plyomgram.post.view.HomeFragment;
 import br.com.plyom.plyomgram.view.fragment.ProfileFragment;
 import br.com.plyom.plyomgram.view.fragment.SearchFragment;
 
 public class ContainerActivity extends AppCompatActivity {
 
+    private static final String TAG = "ContainerActivity";
+
     private Fragment fragment;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+        initFirebase();
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottom_bar);
         bottomBar.setDefaultTab(R.id.home);
@@ -56,4 +73,52 @@ public class ContainerActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mSignOut:
+                signOut();
+                goLogin();
+                break;
+            case R.id.mAbout:
+                Toast.makeText(this, "By JIAE 2017", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Log.i(TAG, "Logged user: " + firebaseUser.getEmail());
+                } else  {
+                    Log.w(TAG, "Not logged user");
+                }
+            }
+        };
+    }
+
+    private void signOut() {
+        firebaseAuth.signOut();
+        if (LoginManager.getInstance() != null) {
+            LoginManager.getInstance().logOut();
+        }
+    }
+
+    private void goLogin() {
+        Intent i = new Intent(ContainerActivity.this, LoginActivity.class);
+        startActivity(i);
+    }
 }
